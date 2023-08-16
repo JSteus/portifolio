a00305c2c1942fdc8ecba8baeddc97d9c7e3b7d0
JSteus <mateus2021r@outlook.com>
2023-08-16 01:22:07 -0300

    [feat] Novo projeto no slider

--- src/utils/index.js
+++ src/utils/index.js
@@ -7,8 +7,15 @@ export const projects = [
     imgUrl: "/images/Pokedex.png",
   },
   {
+    title: "Podcastr",
+    description: "A NextJS Podcast App",
+    demoUrl: null,
+    gitUrl: "https://github.com/JSteus/nlw-podcastr",
+    imgUrl: "/images/Podcastr.png",
+  },
+  {
     title: "TheMovieDB",
-    description: "A Responsive React Movie Catalog ",
+    description: "A Responsive React Movie Catalog",
     demoUrl: null,
     gitUrl: "https://github.com/JSteus/themoviedb",
     imgUrl: "/images/TMDB.png",
